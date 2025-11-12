b41f0ccc9e94bca87308067b8ed72bc7144f7e94
Jeff Wainwright <1074042+yowainwright@users.noreply.github.com>
2025-11-11 23:04:07 -0800

    chore: fixes format issues

--- tests/fixtures/false-positive-minified.js
+++ tests/fixtures/false-positive-minified.js
@@ -1,2 +1,6 @@
-function calculate(e){return e.isRemovedItem?.35:1}
-function another(x){return x.active?.5:.25}
+function calculate(e) {
+  return e.isRemovedItem ? 0.35 : 1;
+}
+function another(x) {
+  return x.active ? 0.5 : 0.25;
+}
